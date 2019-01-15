e1e37050dd310ca4e560cf730695d62bf61e405c
Leonhard S <leonhard-s@users.noreply.github.com>
2019-01-15 14:12:48 +0100

    Conformed `loadout.py`

--- auraxium/ps2/loadout.py
+++ auraxium/ps2/loadout.py
@@ -1,37 +1,46 @@
 from ..census import _CENSUS_BASE_URL, Query
-from ..datatypes import InterimDatatype, StaticDatatype
+from ..datatypes import EnumeratedDataType
 from .faction import Faction
 from .profile import Profile
 
 
-class Loadout(StaticDatatype):
+class Loadout(EnumeratedDataType):
     """A loadout in PlanetSide 2.
 
     A loadout is defined by the class and faction of a character. Examples
-    are "TR Infiltrator", "VS Heavy Assault" or "NC MAX". I am uncertain
-    whether these are worth giving access to.
+    are "TR Infiltrator", "VS Heavy Assault" or "NC MAX".
 
     """
 
-    _collection = 'loadout'
-    _join = ['faction', 'profile']
-
-    def __init__(self, id, data_override=None):
+    def __init__(self, id):
         self.id = id
 
-        if super().is_cached(self):  # If the object is cached, skip
-            return
+        # Set default values
+        self._faction_id = None
+        self.name = None
+        self._profile_id = None
 
-        data = data_override if data_override != None else super().get_data(self)
+        # Define properties
+        @property
+        def faction(self):
+            try:
+                return self._faction
+            except AttributeError:
+                self._faction = Faction.get(id=self._faction_id)
+                return self._faction
 
-        self.faction = Faction(data.get('faction_id'),
-                               data_override=data.get('faction'))
-        self.name = data.get('code_name')
-        self.profile = Profile(data.get('profile_id'),
-                               data_override=data.get('profile'))
+        @property
+        def profile(self):
+            try:
+                return self._profile
+            except AttributeError:
+                self._profile = Profile.get(id=self._profile_id)
+                return self._profile
 
-        super()._add_to_cache(self)  # Cache this instance for future use
+    def _populate(self, data_override=None):
+        data = data_override if data_override != None else super().get(self.id)
 
-    def __str__(self):
-        return 'Loadout (ID: {}, Name: "{}")'.format(
-            self.id, self.name)
+        # Set attribute values
+        self._faction_id = data['faction_id']
+        self.name = data['code_name']
+        self._profile_id = data['profile_id']
