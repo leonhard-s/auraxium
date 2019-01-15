7fd211687ccb2a4afe6f15723dd7e743066808c9
Leonhard S <leonhard-s@users.noreply.github.com>
2019-01-15 11:46:10 +0100

    Conformed `achievement.py`

--- auraxium/ps2/achievement.py
+++ auraxium/ps2/achievement.py
@@ -1,12 +1,13 @@
 from ..census import Query
-from ..datatypes import InterimDatatype
+from ..datatypes import CachableDataType
+from ..misc import LocalizedString
 from .image import Image, ImageSet
 from .item import Item
-# from .objective import ObjectiveSet
+# from .objetive import ObjectiveGroup
 from .reward import Reward
 
 
-class Achievement(InterimDatatype):
+class Achievement(CachableDataType):
     """An achievement in PlanetSide 2.
 
     An achievement is a blanket term covering both weapon medals and service
@@ -14,33 +15,70 @@ class Achievement(InterimDatatype):
 
     """
 
-    # Since achievements are quite light-weight and cannot be resolved further,
-    # their cache size can be safely increased.
-    _cache_size = 100
-    _collection = 'achievement'
-    _join = ['item', 'image_set', 'reward']
-
-    def __init__(self, id, data_override=None):
+    def __init__(self, id):
         self.id = id
 
-        if super().is_cached(self):  # If the object is cached, skip
-            return
+        # Set default values
+        self.description = None
+        self._item_id = None
+        self._image_id = None
+        self._image_set_id = None
+        self.name = None
+        # self._objective_group = None  # Always identical to the id?
+        self.repeatable = None
+        self.resource_cast_cost = None
+        self._reward_id = None
 
-        data = data_override if data_override != None else super().get_data(self)
+        # Define properties
+        @property
+        def item(self):
+            try:
+                return self._item
+            except AttributeError:
+                self._item = Item.get(id=self._item_id)
+                return self._item
 
-        self.description = data.get('description')
-        self.item = Item(data.get('item_id'), data_override=data.get('item'))
-        self.image = Image(data.get('image_id'))
-        self.image_set = ImageSet(
-            data.get('image_set_id'), data_override=data.get('image_set'))
-        self.name = data.get('name')
-        # self.objective_group = None  # Identical to objective_set?
+        @property
+        def image(self):
+            try:
+                return self._image:
+                self._image = Image.get(id=self._image_id)
+                return self._image
+
+                @property
+        def image_set(self):
+            try:
+                return self._image_set
+            except AttributeError:
+                self._image_set = ImageSet.get(id=self._image_set_id)
+                return self._image_set
+
+        # @property
+        # def objective_group(self):
+        #     try:
+        #         return self._objective_group
+        #     except AttributeError:
+        #         self._objective_group = ObjectiveGroup.get(
+        #             id=self._objective_group_id)
+        #         return self._objective_group
+
+        @property
+        def reward(self):
+            try:
+                return self._reward
+            except AttributeError:
+                self._reward = Reward.get(id=self._reward_id)
+                return self._reward
+
+    def _populate(self, data_override=None):
+        data = data_override if data_override != None else super().get(self.id)
+
+        # Set attribute values
+        self.description = LocalizedString(data.get('description'))
+        self._item_id = data.get('item_id')
+        self._image_id = data.get('image_id')
+        self._image_set_id = data.get('image_set_id')
+        self.name = LocalizedString(data.get('name'))
+        # self._objective_group_id = data.get('objective_group_id')
         self.repeatable = data.get('repeatable')
-        self.reward = Reward(data.get('reward_id'),
-                             data_override=data.get('reward'))
-
-        super()._add_to_cache(self)  # Cache this instance for future use
-
-    def __str__(self):
-        return 'Achievement (ID: {}, Name[en]: "{}")'.format(
-            self.id, self.name['en'])
+        self._reward_id = data.get('reward_id')
