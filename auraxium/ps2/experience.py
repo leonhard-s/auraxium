fd5b956a27e739b92deb9c240b0a9794e366d50e
Leonhard S <leonhard-s@users.noreply.github.com>
2019-01-15 13:59:29 +0100

    Conformed `experience.py`

--- auraxium/ps2/experience.py
+++ auraxium/ps2/experience.py
@@ -1,24 +1,23 @@
-from ..census import Query
-from ..datatypes import InterimDatatype
+from ..datatypes import CachableDataType
 
 
-class Experience(InterimDatatype):
-    _cache_size = 100
-    _collection = 'experience'
+class Experience(CachableDataType):
+    """An experience type.
 
-    def __init__(self, id, data_override=None):
+    Lists all the actions that provide experience.
+
+    """
+
+    def __init__(self, id):
         self.id = id
 
-        if super().is_cached(self):  # If the object is cached, skip
-            return
+        # Set default values
+        self.amount = None
+        self.description = None
 
-        data = data_override if data_override != None else super().get_data(self)
+    def _populate(self, data_override=None):
+        data = data_override if data_override != None else super().get(self.id)
 
-        self.amount = data.get('xp')
-        self.description = data.get('description')
-
-        super()._add_to_cache(self)  # Cache this instance for future use
-
-    def __str__(self):
-        return 'Experience (ID: {}, Description: "{}")'.format(
-            self.id, self.description)
+        # Set attribute values
+        self.amount = data['xp']
+        self.description = data['description']
