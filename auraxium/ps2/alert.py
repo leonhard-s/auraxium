ecd25ed2575c3dccf469e4c52ca141ea0e3b2e1b
Leonhard S <leonhard-s@users.noreply.github.com>
2019-01-15 14:19:27 +0100

    Conformed `alert.py`

--- auraxium/ps2/alert.py
+++ auraxium/ps2/alert.py
@@ -1,52 +1,55 @@
 from ..census import Query
-from ..datatypes import StaticDatatype
+from ..datatypes import EnumeratedDataType
+from ..misc import LocalizedString
 
 
-class AlertState(StaticDatatype):
-    _collection = 'metagame_event_state'
+class AlertState(EnumeratedDataType):
+    """The state of an alert.
 
-    def __init__(self, id, data_override=None):
+    Lists the states an alert can be in, like "started".
+
+    """
+
+    def __init__(self, id):
         self.id = id
 
-        if super().is_cached(self):  # If the object is cached, skip
-            return
+        # Set default values
+        self.name = None
 
-        data = data_override if data_override != None else super().get_data(self)
+    def _populate(self, data_override=None):
+        data = data_override if data_override != None else super().get(self.id)
 
-        self.name = data.get('name')
-
-        super()._add_to_cache(self)  # Cache this instance for future use
-
-    def __str__(self):
-        return 'AlertState (ID: {}, Name: "{}")'.format(self.id, self.name)
+        # Set attribute values
+        self.name = data['name']
 
 
-class AlertType(StaticDatatype):
-    _collection = 'metagame_event'
+class Alert(EnumeratedDataType):
+    """An alert/event.
 
-    def __init__(self, id, data_override=None):
+    An alert that can take place on Auraxis. Not all event types are
+    currently enabled in-game.
+
+    """
+
+    def __init__(self, id):
         self.id = id
 
-        if super().is_cached(self):  # If the object is cached, skip
-            return
+        # Set default values
+        self.description = None
+        self.experience_bonus = None
+        self.name = None
+        self.type = None
 
-        data = data_override if data_override != None else super().get_data(self)
+    def _populate(self, data_override=None):
+        data = data_override if data_override != None else super().get(self.id)
 
-        self.description = data.get('description')
-        self.experience_bonus = data.get('experience_bonus')
-        self.name = data.get('name')
-
-        # Hard-coded descriptions of the base alert types
-        # 1 and 6 are currently unused.
-        base_alert_types = {'1': 'Territory Control', '2': 'Facility Type',
+        # Set attribute values
+        self.description = LocalizedString(data['description'])
+        self.experience_bonus = data['experience_bonus']
+        self.name = LocalizedString(data['name'])
+        # Hard-coded descriptions of the base alert types, 1 and 6 are unused.
+        alert_types = {'1': 'Territory Control', '2': 'Facility Type',
                             '5': 'Warpgates Stabilizing', '6': 'Conquest',
                             '8': 'Meltdown', '9': 'Unstable Meltdown',
                             '10': 'Aerial Anomalies'}
-
-        self.type = base_alert_types[data.get('type')]
-
-        super()._add_to_cache(self)  # Cache this instance for future use
-
-    def __str__(self):
-        return 'AlertType (ID: {}, Name[en]: "{}")'.format(
-            self.id, self.name['en'])
+        self.type = alert_types[data['type']]
