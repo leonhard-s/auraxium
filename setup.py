a677e5b13f654450e35d12fc9f440267d4c1e87f
Leonhard S <leonhard-s@users.noreply.github.com>
2019-02-22 15:56:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import setuptools
 
-setuptools.setup(
-    name="auraxium",
-    version="0.0.1",
-    author="Leonhard S.",
-    author_email="leonhard-s@users.noreply.github.com",
-    description="A python wrapper for the PlanetSide 2 Census API.",
-    long_description=('# Auraxium\n\nA Python wrapper meant to facilitate the '
-                      'use of the [Daybreak Game Company Census API]'
-                      '(https://census.daybreakgames.com/).'),
-    long_description_content_type="text/markdown",
-    url="https://github.com/leonhard-s/auraxium",
-    packages=setuptools.find_packages(),
-    classifiers=["Programming Language :: Python :: 3",
-                 "License :: OSI Approved :: MIT License",
-                 "Operating System :: OS Independent"]
-)
+setuptools.setup(name='auraxium',
+                 version='0.0.2',
+                 author='Leonhard S.',
+                 author_email='leonhard-s@users.noreply.github.com',
+                 description='A python wrapper for the PlanetSide 2 Census API.',
+                 long_description=('# Auraxium\n\nA Python wrapper meant to facilitate the '
+                                   'use of the [Daybreak Game Company Census API]'
+                                   '(https://census.daybreakgames.com/).'),
+                 long_description_content_type='text/markdown',
+                 keywords='auraxium python daybreak census planetside ps2',
+                 url='https://github.com/leonhard-s/auraxium',
+                 packages=setuptools.find_packages(),
+                 classifiers=['Development Status :: 3 - Alpha',
+                              'Programming Language :: Python :: 3',
+                              'License :: OSI Approved :: MIT License',
+                              'Operating System :: OS Independent'],
+                 license='MIT',
+                 include_package_data=True,
+                 zip_safe=False)
