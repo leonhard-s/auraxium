880ee9ad919bb62a73c26e87fc3ad7763d95b008
Leonhard S <leonhard-sei@outlook.com>
2020-06-18 14:56:27 +0200

    Add currency module
    This should be merged with other character or resource information in the future.

---
+++ auraxium/ps2/currency.py
@@ -0,0 +1,40 @@
+"""Currency class definition."""
+
+import dataclasses
+
+from ..base import Cached, Ps2Data
+from ..types import CensusData
+from ..utils import LocaleData
+
+
+@dataclasses.dataclass(frozen=True)
+class CurrencyData(Ps2Data):
+    """Data class for :class:`auraxium.ps2.currency.Currency`.
+
+    This class mirrors the payload data returned by the API, you may
+    use its attributes as keys in filters or queries.
+    """
+
+    currency_id: int
+    name: LocaleData
+    icon_id: int
+    inventory_cap: int
+
+    @classmethod
+    def from_census(cls, data: CensusData) -> 'CurrencyData':
+        return cls(
+            int(data['currency_id']),
+            LocaleData.from_census(data['name']),
+            int(data['icon_id']),
+            int(data['inventory_cap']))
+
+
+class Currency(Cached, cache_size=10, cache_ttu=3600.0):
+    """A currency obtainable by characters."""
+
+    _collection = 'currency'
+    data: CurrencyData
+    _id_field = 'currency_id'
+
+    def _build_dataclass(self, data: CensusData) -> CurrencyData:
+        return CurrencyData.from_census(data)
