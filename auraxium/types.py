581c05be32b0d9ed21da148638f51e5727003066
Leonhard S <leonhard-sei@outlook.com>
2020-06-28 21:12:25 +0200

    Fix typo

--- auraxium/types.py
+++ auraxium/types.py
@@ -4,6 +4,6 @@ from typing import Any, Dict
 
 __all__ = ['CensusData']
 
-# NOTE: This is types as "Any" due to Mypy not supporting cycling definitions
+# NOTE: This is typed as "Any" due to Mypy not supporting cycling definitions
 # yet.
 CensusData = Dict[str, Any]
