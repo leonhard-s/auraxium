4bf7f504dde02f3bc7bfcf43eb068f98672ffed7
Leonhard S <leonhard-sei@outlook.com>
2020-08-15 12:51:53 +0200

    Unify object model imports

--- auraxium/ps2/__init__.py
+++ auraxium/ps2/__init__.py
@@ -5,16 +5,89 @@ census. Note that the object attributes and hierarchy of this object
 model will not match up to the Census API perfectly.
 """
 
+from .ability import Ability, AbilityType
+from .achievement import Achievement
+from .armour import ArmourFacing, ArmourInfo
 from .character import Character
+from .currency import Currency
+from .depot import MarketingBundle, MarketingBundleSingle
+from .directive import (Directive, DirectiveTier, DirectiveTree,
+                        DirectiveTreeCategory)
+from .effect import Effect, EffectType
+from .experience import Experience, ExperienceRank
 from .faction import Faction
-from .item import Item
-from .weapon import Weapon
+from .fire import FireGroup, FireMode, FireModeType
+from .item import Item, ItemCategory, ItemType
+from . import leaderboard
+from .map import MapHex, MapRegion, Region
+from .metagame import MetagameEvent, MetagameEventState
+from .objective import Objective, ObjectiveType
+from .outfit import Outfit, OutfitMember
+from .projectile import Projectile, ProjectileFlightType
+from .resist import ResistInfo, ResistType
+from .reward import Reward, RewardType
+from .skill import Skill, SkillCategory, SkillLine, SkillSet
+from .states import PlayerState, PlayerStateGroup
+from .vehicle import Vehicle, VehicleAttachment
+from .weapon import Weapon, WeaponAmmoSlot, WeaponDatasheet
 from .world import World
+from .zone import Zone
+from .zone_effect import ZoneEffect, ZoneEffectType
 
 __all__ = [
+    'Ability',
+    'AbilityType',
+    'Achievement',
+    'ArmourFacing',
+    'ArmourInfo',
     'Character',
+    'Currency',
+    'MarketingBundle',
+    'MarketingBundleSingle',
+    'Directive',
+    'DirectiveTier',
+    'DirectiveTree',
+    'DirectiveTreeCategory',
+    'Effect',
+    'EffectType',
+    'Experience',
+    'ExperienceRank',
     'Faction',
+    'FireGroup',
+    'FireMode',
+    'FireModeType',
     'Item',
+    'ItemCategory',
+    'ItemType',
+    'leaderboard',
+    'MapHex',
+    'MapRegion',
+    'Region',
+    'MetagameEvent',
+    'MetagameEventState',
+    'Objective',
+    'ObjectiveType',
+    'Outfit',
+    'OutfitMember',
+    'Projectile',
+    'ProjectileFlightType',
+    'ResistInfo',
+    'ResistType',
+    'Reward',
+    'RewardType',
+    'Skill',
+    'SkillCategory',
+    'SkillLine',
+    'SkillSet',
+    'PlayerState',
+    'PlayerStateGroup',
+    'Vehicle',
+    'VehicleAttachment',
     'Weapon',
-    'World'
+    'WeaponAmmoSlot',
+    'WeaponDatasheet',
+    'World',
+    'Zone',
+    'ZoneEffect',
+    'ZoneEffectType'
 ]
