82b8a8168d3ba46ed81646a9dfb43d45a58af253
Leonhard S <leonhard-sei@outlook.com>
2020-10-08 22:32:07 +0200

    Event client unit test
    Initial testing framework for the ESS client. More tests to be added soon, this is mostly a proof-of-concept for using the GitHub secrets to set up the testing environment.

---
+++ tests/event_test.py
@@ -0,0 +1,86 @@
+"""Tests for the real-time event client module."""
+
+import asyncio
+import unittest
+from typing import Any, Optional
+
+# pylint: disable=import-error
+import auraxium
+from tests.utils import SERVICE_ID
+
+
+@unittest.skipIf(SERVICE_ID == 's:example', 'missing service ID')
+class EventClientTest(unittest.IsolatedAsyncioTestCase):
+    """Live tests for the real-time event client component."""
+
+    # Infrastructure
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._client: Optional[auraxium.EventClient] = None
+
+    @property
+    def client(self) -> auraxium.EventClient:
+        """Shorthand for returning the active client."""
+        assert self._client is not None, 'test setup issue'
+        return self._client
+
+    async def asyncSetUp(self) -> None:
+        """Reset the event streaming client before every test."""
+        if self._client is not None:
+            await self._client.close()
+        self._client = auraxium.EventClient(service_id=SERVICE_ID)
+
+    async def asyncTearDown(self) -> None:
+        """Close the event streaming client after every test."""
+        if self._client is not None:
+            await self._client.close()
+        self._client = None
+
+    # Test cases
+
+    def test_startup_defensive(self) -> None:
+        """Ensure the client does not connect without any triggers."""
+        self.assertFalse(self.client.triggers, 'predefined triggers')
+        self.assertIsNone(self.client.websocket, 'websocket without triggers')
+
+    async def test_connect_on_trigger(self) -> None:
+        """Ensure the client connects as soon as a trigger is added."""
+        self.assertFalse(self.client.triggers, 'predefined triggers found')
+        self.assertIsNone(self.client.websocket, 'preexisting websocket found')
+        # Add trigger and wait for ready
+        trigger = auraxium.Trigger(auraxium.EventType.BATTLE_RANK_UP)
+        self.client.add_trigger(trigger)
+        await self.client.wait_ready()
+        # Check for websocket activity
+        self.assertIsNotNone(self.client.websocket, 'missing websocket')
+        self.assertListEqual(
+            self.client.triggers, [trigger], 'trigger not found')
+
+    async def test_messages(self) -> None:
+        """Test event dispatching."""
+        flag = asyncio.Event()
+
+        async def on_death(event: auraxium.Event) -> None:
+            self.assertIsInstance(event, auraxium.Event, 'non-event returned')
+            flag.set()
+
+        self.client.trigger(auraxium.EventType.DEATH)(on_death)
+        await flag.wait()
+        self.assertEqual(len(self.client.triggers), 1)
+        self.client.remove_trigger('on_death')
+        self.assertEqual(len(self.client.triggers), 0)
+
+    async def test_single_shot(self) -> None:
+        """Test a single-shot trigger to ensure it is auto-deleted."""
+        trigger = auraxium.Trigger(auraxium.EventType.DEATH, single_shot=True)
+        flag = asyncio.Event()
+
+        async def wait_for(event: auraxium.Event) -> None:
+            _ = event
+            flag.set()
+
+        trigger.action = wait_for
+        self.client.add_trigger(trigger)
+        await flag.wait()
+        self.assertEqual(len(self.client.triggers), 0)
