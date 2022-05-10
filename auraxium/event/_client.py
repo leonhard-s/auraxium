38a8149157688f3f0195bb3f06df9d44bfa0c439
Leonhard S <leonhard-sei@outlook.com>
2022-05-10 13:14:46 +0200

    Add flag for ignoring expired SSL certs
    DBG regularly lets the API certs expire, causing the Auraxium Websocket connection to fail by default. This adds the "no_ssl_certs" flag that disables certificate checks as a workaround. See #55.

--- auraxium/event/_client.py
+++ auraxium/event/_client.py
@@ -2,6 +2,7 @@ import asyncio
 import contextlib
 import json
 import logging
+import ssl
 from typing import (Any, Callable, Coroutine, Dict, Iterator, List, Optional, Type, TypeVar, Union,
                     cast, overload)
 
@@ -264,7 +265,11 @@ class EventClient(Client):
         """
         _log.info('Connecting to WebSocket endpoint...')
         url = f'{_ESS_ENDPOINT}?environment=ps2&service-id={self.service_id}'
-        async with ws_client.connect(url) as websocket:
+        if self._no_ssl_certs:
+            ssl_ctx = ssl.SSLContext()
+        else:
+            ssl_ctx = None
+        async with ws_client.connect(url, ssl=ssl_ctx) as websocket:
             self.websocket = websocket
             _log.info('Connected to %s?environment=ps2&service-id=XXX',
                       _ESS_ENDPOINT)
