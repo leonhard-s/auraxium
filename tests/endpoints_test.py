46af7abd7dbd5df1facb6709690b78e4d34f4cd9
Leonhard S <leonhard-sei@outlook.com>
2022-09-10 12:28:00 +0200

    Add ess_endpoint argument for EventClient

--- tests/endpoints_test.py
+++ tests/endpoints_test.py
@@ -14,8 +14,8 @@ class EndpointConfigurationTest(unittest.TestCase):
         """Ensure the PS2 endpoints are used by default."""
         client = auraxium.Client()
         self.assertListEqual(client.endpoints, [endpoints.DBG_CENSUS])
-        # client = auraxium.EventClient()
-        # client.ess_endpoint = endpoints.DBG_STREAMING
+        client = auraxium.EventClient()
+        client.ess_endpoint = endpoints.DBG_STREAMING
 
     def test_custom(self) -> None:
         """Ensure a single custom endpoint overrides as intended."""
@@ -29,3 +29,20 @@ class EndpointConfigurationTest(unittest.TestCase):
         url = str(dummy.query().url())
         self.assertEqual(url, 'https://census.lithafalcon.cc/get/'
                          'ps2:v2/fire_group?fire_group_id=0')
+
+
+class LiveEndpointTest(unittest.IsolatedAsyncioTestCase):
+    """Live tests for custom community endpoints."""
+
+    client: auraxium.EventClient
+
+    async def asyncTearDown(self) -> None:
+        await super().asyncTearDown()
+        await self.client.close()
+
+    async def test_custom_rest(self) -> None:
+        self.client = auraxium.EventClient(
+            endpoints=[endpoints.SANCTUARY_CENSUS])
+        result = await self.client.get_by_id(ps2.Faction, 4)
+        if result is None:
+            self.skipTest('Endpoint not available')
