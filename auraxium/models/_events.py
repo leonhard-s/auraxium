3b2cd5779c0dd98357bb57bbcba85c9f5ce59ed3
nlioc4 <colinvanderpol@gmail.com>
2023-07-05 09:23:46 -0700

    Add TeamID to VehicleDestroy and Death event models

--- auraxium/models/_events.py
+++ auraxium/models/_events.py
@@ -141,6 +141,11 @@ class Death(Event, CharacterEvent):
          The reference above is not an error, this field reports the
          item ID of the weapon, not the weapon ID.
 
+    .. attribute:: attacker_team_id
+         :type: int
+
+            ID of the team of the attacker.
+
     .. attribute:: character_id
        :type: int
 
@@ -194,6 +199,7 @@ class Death(Event, CharacterEvent):
     attacker_loadout_id: int
     attacker_vehicle_id: int
     attacker_weapon_id: int
+    attacker_team_id: int
     character_id: int
     character_loadout_id: int
     is_critical: Optional[bool]  # Always false
@@ -656,6 +662,11 @@ class VehicleDestroy(Event, CharacterEvent):
          The reference above is not an error, this field reports the
          item ID of the weapon, not the weapon ID.
 
+    .. attribute:: attacker_team_id
+         :type: int
+
+         The ID of the team of the attacker
+
     .. attribute:: character_id
        :type: int
 
@@ -704,6 +715,7 @@ class VehicleDestroy(Event, CharacterEvent):
     attacker_loadout_id: int
     attacker_vehicle_id: int
     attacker_weapon_id: int
+    attacker_team_id: int
     character_id: int
     facility_id: int
     faction_id: int
