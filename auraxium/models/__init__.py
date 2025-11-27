6b17ca15e6b5931f7afc460fd1e3cfefdc1c1ece
Leonhard S. <leonhard-sei@outlook.com>
2025-11-27 11:08:57 +0300

    Drop models for PS2 payloads
    The REST OpenAPI spec does not model the ESS payloads, so those need to stick around for now.

--- auraxium/models/__init__.py
+++ auraxium/models/__init__.py
@@ -1,122 +1,27 @@
 """Python representations of PlanetSide 2 payloads."""
 
-from ._ability import AbilityData, AbilityTypeData, ResourceTypeData
-from ._achievement import AchievementData
-from ._armour import ArmourInfoData
 from .base import CharacterEvent, Event, WorldEvent
-from ._character import (CharacterAchievement, CharacterData,
-                         CharacterDirective, CharacterDirectiveObjective,
-                         CharacterDirectiveTier, CharacterDirectiveTree,
-                         TitleData)
-from ._currency import CurrencyData
-from ._depot import MarketingBundleData, MarketingBundleSingleData
-from ._directive import (DirectiveData, DirectiveTierData,
-                         DirectiveTreeCategoryData, DirectiveTreeData)
-from ._effect import (EffectData, EffectTypeData, ZoneEffectData,
-                      ZoneEffectTypeData)
 from ._events import (AchievementAdded, BattleRankUp, Death, FacilityControl,
                       GainExperience, ItemAdded, MetagameEvent,
                       PlayerFacilityCapture, PlayerFacilityDefend, PlayerLogin,
                       PlayerLogout, SkillAdded, VehicleDestroy, ContinentLock)
-from ._experience import (ExperienceAwardTypeData, ExperienceData,
-                          ExperienceRankData)
-from ._faction import FactionData
-from ._fire import FireGroupData, FireModeData
-from ._fish import FishData
-from ._item import ItemCategoryData, ItemData, ItemTypeData
-from ._map import FacilityTypeData, MapHexData, MapRegionData, RegionData
-from ._metagame import MetagameEventData
-from ._objective import ObjectiveData, ObjectiveTypeData
-from ._outfit import OutfitData, OutfitMemberData, OutfitRankData
-from ._profile import LoadoutData, ProfileData
-from ._projectile import ProjectileData
-from ._resist import ResistInfoData, ResistTypeData
-from ._reward import RewardData, RewardTypeData
-from ._skill import SkillData, SkillCategoryData, SkillLineData, SkillSetData
-from ._states import PlayerStateGroup
-from ._vehicle import VehicleAttachmentData, VehicleData
-from ._weapon import WeaponAmmoSlot, WeaponData, WeaponDatasheet
-from ._world import WorldData
-from ._zone import ZoneData
 
 __all__ = [
-    'AbilityData',
-    'AbilityTypeData',
     'AchievementAdded',
-    'AchievementData',
-    'ArmourInfoData',
     'BattleRankUp',
-    'CharacterAchievement',
-    'CharacterData',
-    'CharacterDirective',
-    'CharacterDirectiveObjective',
-    'CharacterDirectiveTier',
-    'CharacterDirectiveTree',
     'CharacterEvent',
     'ContinentLock',
-    'CurrencyData',
     'Death',
-    'DirectiveData',
-    'DirectiveTierData',
-    'DirectiveTreeCategoryData',
-    'DirectiveTreeData',
-    'EffectData',
-    'EffectTypeData',
-    'ExperienceAwardTypeData',
-    'ExperienceData',
-    'ExperienceRankData',
     'Event',
     'FacilityControl',
-    'FacilityTypeData',
-    'FactionData',
-    'FireGroupData',
-    'FireModeData',
-    'FishData',
     'GainExperience',
     'ItemAdded',
-    'ItemCategoryData',
-    'ItemData',
-    'ItemTypeData',
-    'LoadoutData',
-    'MapHexData',
-    'MapRegionData',
-    'MarketingBundleData',
-    'MarketingBundleSingleData',
     'MetagameEvent',
-    'MetagameEventData',
-    'ObjectiveData',
-    'ObjectiveTypeData',
-    'OutfitData',
-    'OutfitMemberData',
-    'OutfitRankData',
     'PlayerFacilityCapture',
     'PlayerFacilityDefend',
     'PlayerLogin',
     'PlayerLogout',
-    'PlayerStateGroup',
-    'ProfileData',
-    'ProjectileData',
-    'RegionData',
-    'ResistInfoData',
-    'ResistTypeData',
-    'ResourceTypeData',
-    'RewardData',
-    'RewardTypeData',
     'SkillAdded',
-    'SkillData',
-    'SkillCategoryData',
-    'SkillLineData',
-    'SkillSetData',
-    'TitleData',
-    'VehicleAttachmentData',
-    'VehicleData',
     'VehicleDestroy',
-    'WeaponAmmoSlot',
-    'WeaponData',
-    'WeaponDatasheet',
-    'WorldData',
     'WorldEvent',
-    'ZoneData',
-    'ZoneEffectData',
-    'ZoneEffectTypeData'
 ]
